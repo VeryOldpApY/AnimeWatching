e62afec457632613a2d01c1c2c1d78b0620fbdfb
Very Old pApY <dalix.playgamer@gmail.com>
2022-08-21 23:11:05 +0200

    V0.4.2
    Update Setup

--- AnimeWatching/MainWindow.xaml.cs
+++ AnimeWatching/MainWindow.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using Microsoft.Web.WebView2.Core;
 
