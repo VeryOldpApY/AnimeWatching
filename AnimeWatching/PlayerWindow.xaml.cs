8027224b44b48de007448ee9a7a1e691b9101d04
Very Old pApY <dalix.playgamer@gmail.com>
2022-08-21 07:46:03 +0200

    https://github.com/VeryOldpApY/AnimeWatching/issues/4

--- AnimeWatching/PlayerWindow.xaml.cs
+++ AnimeWatching/PlayerWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Microsoft.Web.WebView2.Wpf;
 
 namespace AnimeWatching
 {
@@ -43,34 +32,28 @@ namespace AnimeWatching
 
 		private void Label_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
 		{
-			this.DragMove();
+			DragMove();
 		}
 
 		private void Bt_Close_Click(object sender, RoutedEventArgs e)
 		{
-			this.Close();
+			Close();
 		}
         private void Bt_Max_Click(object sender, RoutedEventArgs e)
         {
-			Label_PreviewMouseDoubleClick(sender, null);
+            if(WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else if(WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
         }
 
         private void Bt_Min_Click(object sender, RoutedEventArgs e)
         {
-            this.WindowState = WindowState.Minimized;
+            WindowState = WindowState.Minimized;
         }
-
-
-        private void Label_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
-		{
-			if (this.WindowState == WindowState.Maximized)
-			{
-				this.WindowState = WindowState.Normal;
-			}
-			else if (this.WindowState == WindowState.Normal)
-			{
-				this.WindowState = WindowState.Maximized;
-			}
-		}
 	}
 }
